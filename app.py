b6dd9338f4d2fe37306326814adeccb8e69de757
sebas <sebas084garciahenao@gmail.com>
2023-08-11 08:51:45 -0500

    punto2_cordenadas

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from Vehiculo import vehiculo
-velocidad=120
+# from Vehiculo import vehiculo
 
-kilometro=50000
+# velocidad=120
 
-print(vehiculo(velocidad,kilometro))
+# kilometro=50000
+
+# print(vehiculo(velocidad,kilometro))
+
+
+
+from punto import punto_pano
+paraX=int(input("ingrese una cordenada: "))
+paraY=int(input("ingrese una cordenada: "))
+
+print(punto_pano(paraX,paraY))
+        
---
+++ punto.py
@@ -0,0 +1,8 @@
+class punto_pano:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        
+        print(f"Las coordenadas asignadas son: {x},{y}")
+
+
