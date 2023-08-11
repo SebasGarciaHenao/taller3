98684a9e29125531738c486e7cb56f31972cc75b
sebas <sebas084garciahenao@gmail.com>
2023-08-11 08:37:24 -0500

    punto1_vehiculo

---
+++ Vehiculo.py
@@ -0,0 +1,10 @@
+class vehiculo:
+    def __init__(self,velocidad_maxima,kilometraje):
+           self.velocidad_maxima=velocidad_maxima
+           self.kilometraje=kilometraje
+    
+           print(f"La velocidad maxima del vehiculo es: {velocidad_maxima} y su kilometraje es: {kilometraje} ")
+
+
+
+
---
+++ app.py
@@ -0,0 +1,6 @@
+from Vehiculo import vehiculo
+velocidad=120
+
+kilometro=50000
+
+print(vehiculo(velocidad,kilometro))
